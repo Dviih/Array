30d3a7e0e90ec87ae88deaaee4d0fa00bdca1b84
Dviih <dviih2s@gmail.com>
2024-01-28 21:22:02 -0300

    Remove data from certain index in map and change indexes from following elements.

--- sync.go
+++ sync.go
@@ -22,3 +22,17 @@ func (syncArray *SyncArray[T]) Append(t T) bool {
 	return true
 }
 
+func (syncArray *SyncArray[T]) Remove(i int) bool {
+	if syncArray == nil {
+		return false
+	}
+
+	syncArray.m.Del(i)
+
+	for ; i < len(syncArray.m.Data); i++ {
+		syncArray.m.Push(i-1, syncArray.Index(i))
+	}
+
+	return true
+}
+
