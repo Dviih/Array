6b551abd4b817d0c7a0de4e604e1fbb560f91c0f
Dviih <dviih2s@gmail.com>
2024-01-28 21:23:51 -0300

    Create a new channel

--- chan.go
+++ chan.go
@@ -9,3 +9,9 @@ type ChanArray[T Interface[T]] struct {
 	channels syncmap.SyncMap[string, chan T]
 }
 
+func (chanArray *ChanArray[T]) Create(name string, size int) chan T {
+	c := make(chan T, size)
+	chanArray.channels.Add(name, c)
+	return c
+}
+
